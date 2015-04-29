59c25e99db2cde8c11d25e404603c3ea6d00f5bb
Justin Huntington <corusco@gmail.com>
2015-04-29 14:48:42 -0600

    Completed through part 4. Adds titles.

---
+++ Recipe App/RecipesTableViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  RecipesTableViewDataSource.h
+//  Recipe App
+//
+//  Created by Justin Huntington on 4/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RecipesTableViewDataSource : NSObject
+
+@end
---
+++ Recipe App/RecipesTableViewDataSource.m
@@ -0,0 +1,13 @@
+//
+//  RecipesTableViewDataSource.m
+//  Recipe App
+//
+//  Created by Justin Huntington on 4/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "RecipesTableViewDataSource.h"
+
+@implementation RecipesTableViewDataSource
+
+@end
